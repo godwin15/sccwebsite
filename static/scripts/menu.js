09149d81749becb3416f62abe03b97fe5ce21568
Godwin Silayo <godwinsilayo100@gmail.com>
2023-06-19 17:42:08 -0400

    mobile menu bar

--- static/scripts/menu.js
+++ static/scripts/menu.js
@@ -1,9 +1,9 @@
-function myFuction() {
-    var x = getElementById("myLinks");
-    if (x.style.display == "block"){
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
         x.style.display = "none";
-    }
-    else{
-        x.style.display = "block";
+    } else {
+        x.style.display = "flex";
+        x.style.flexDirection = "column";
     }
 }
