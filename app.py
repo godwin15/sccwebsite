87c02f9d98ce2d4ef047f69205dd6650c1d86976
Ayeyiiat <asaturks24@gmail.com>
2023-06-21 15:04:32 -0400

    Made progress on switching in between pages

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, template_folder="html/", static_url_path="/static")
 def home_page():
     return render_template('index.html')
 
+@app.route('/joinus')
+def joinus_page():
+    return render_template('join_us.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
