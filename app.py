bc3984d120393590d4ad693a83ff57bcaf6b16e2
Ayeyiiat <asaturks24@gmail.com>
2023-06-22 21:27:59 -0400

    made the menu bar show on all pages

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app = Flask(__name__, template_folder="html/", static_url_path="/static")
 def home_page():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
 @app.route('/joinus')
 def joinus_page():
     return render_template('join_us.html')
+
+@app.route('/materials')
+def materials():
+    return render_template('materials.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
